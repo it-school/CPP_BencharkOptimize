f2b0368ab9f092f8f1a14f19a09f284f5ecd42e4
vencedor <it@osvita.od.ua>
2023-10-20 15:07:03 +0300

    Explanations added

--- CPP_BencharkOptimize.cpp
+++ CPP_BencharkOptimize.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <chrono>
 
-int main()
-{
-	const long long OPERATIONS = 1000000000; // 10 000 000 000
-	/*
+const long long OPERATIONS = 1000000000; // 10 000 000 000
+
+/// <summary>
+/// Comparing loops performance with diferent counter types, conditions and run format
+/// </summary>
+void example1() {
 	long long x = 0, y = 0;
 
 	// ---------------------------------  1  ---------------------------------
@@ -30,25 +32,33 @@ int main()
 
 	std::cout << std::endl << "Calculation result: " << y << std::endl;
 	std::cout << "Execution time: " << executionTime << " ms" << std::endl;
+}
 
-	*/
-	
-	// y = 5*x^7 - 2.5*x^6 + 3*x^5 - 8.3*x^4 + 5*x^3 - 9*x^2 + 10*x-2;
+/// <summary>
+/// Let's solve the expression calculated in billion cycles loop and optimize it to improve the performance
+/// y = 5*x^7 - 2.5*x^6 + 3*x^5 - 8.3*x^4 + 5*x^3 - 9*x^2 + 10*x-2;
+/// </summary>
+void example2()
+{
 	double y, a, b;
 	auto startTime = std::chrono::high_resolution_clock::now();
-	for (size_t i = 0; i < 10; i++)
+	for (char i = 0; i < 10; i++)
 	{
 		for (long long x = 0; x < OPERATIONS; x++)
 		{
 			// y = 5 * pow(x, 7) - 2.5 * pow(x, 6) + 3 * pow(x, 5) - 8.3 * pow(x, 4) + 5 * pow(x, 3) - 9 * pow(x, 2) + 10 * x - 2;  // 10470 ms
 			// y = 5 * x*x*x*x*x*x*x - 2.5 * x*x*x*x*x*x + 3 * x*x*x*x*x - 8.3 * x*x*x*x + 5 * x*x*x - 9 * x*x + 10 * x - 2;  // 70 ms
 
-/*			a = x * x;
-			b = x * x * x;
-			// y = 5 * b*b*x - 2.5 * b*b + 3 * b*a - 8.3 * b*x + 5 * b - 9 * a + 10 * x - 2;// 36 ms
-			y = a * (a * (5 * b - 8.3) + 3 * b - 9) + b * (5 - 2.5 * b) + 10 * x - 2; // 20 ms
-*/
+// Optimization of calculations
 
+// 1st way
+//			a = x * x;
+//			b = x * x * x;
+//			y = 5 * b*b*x - 2.5 * b*b + 3 * b*a - 8.3 * b*x + 5 * b - 9 * a + 10 * x - 2;		// 36 ms
+//			y = a * (a * (5 * b - 8.3) + 3 * b - 9) + b * (5 - 2.5 * b) + 10 * x - 2;			// 20 ms
+
+
+// 2nd way
 //			y = x * (5 * x * x * x * x * x * x - 2.5 * x * x * x * x * x + 3 * x * x * x * x - 8.3 * x * x * x + 5 * x * x - 9 * x + 10) - 2; //38 ms
 //			y = x * (x * (5 * x * x * x * x * x - 2.5 * x * x * x * x + 3 * x * x * x - 8.3 * x * x + 5 * x - 9) + 10) - 2; //32 ms
 //			y = x * (x * (x * (5 * x * x * x * x - 2.5 * x * x * x + 3 * x * x - 8.3 * x + 5) - 9) + 10) - 2;	// 28 ms
@@ -62,4 +72,10 @@ int main()
 
 	std::cout << std::endl << "Calculation result: " << y << std::endl;
 	std::cout << "Execution time: " << executionTime << " ms" << std::endl;
+}
+
+int main()
+{
+	example1();
+	example2();
 }
